712b2b28f016f4b6b4121fded7ed3a00f52ac994
bydaniel12 <djuchiha12@gmail.com>
2021-10-07 19:05:03 +0200

    Mejora MObile fecha

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -182,7 +182,7 @@ const Detalle = () => {
     const formatDate = (paramFecha) =>{
         const fecha = new Date(paramFecha);
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return fecha.toLocaleDateString("es-PE", options);
+        return fecha.toLocaleDateString(undefined, options);
     }
 
     return (
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -130,7 +130,7 @@ const Inicio = () => {
     const formatDate = (paramFecha) =>{
         const fecha = new Date(paramFecha);
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        return fecha.toLocaleDateString("es-PE", options);
+        return fecha.toLocaleDateString(undefined, options);
     }
 
     return (
